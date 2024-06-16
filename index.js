e7b606fc759d64447a8de8816fc0b45957fd4802
mariamarcela <maria.marcela.sousa@gmail.com>
2024-06-16 19:53:00 -0300

    Alterações

--- index.js
+++ index.js
@@ -12,9 +12,13 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 // Conectar ao MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+  .catch(err => console.error('MongoDB connection error:', err));
+
 
 // Rotas
 app.use('/api/products', productRoutes);
